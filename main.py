5dde18b36ad8f914cab9522633ce1a09b4a14ce6
yasrirazak <31167110+yasrirazak@users.noreply.github.com>
2024-08-07 09:47:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pickle
 import pandas as pd
 import streamlit as st
+from sklearn import linear_model
 logr = pickle.load(open('model.pkl','rb'))
-
+def main(): 
+  age = st.text_input("Sex","0") 
+  hb = st.text_input("Hb","0")
+  if st.button("Predict"):
+    x_test_data = [[age,hb]]
+    X_test = pd.DataFrame(x_test_data, columns=['Sex', 'Hb'])
+    predicted = logr.predict(X_test)
+    st.success('Predicted {}'.format(predicted))
+if __name__=='__main__': 
+    main() 
     
