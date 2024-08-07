523a2651f3b146190b62f0e4adf05489986efc4d
yasrirazak <31167110+yasrirazak@users.noreply.github.com>
2024-08-07 09:30:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pickle
 import pandas as pd
 import streamlit as st
 logr = pickle.load(open('model.pkl','rb'))
-x_test_data = [[1.0,14]]
-X_test = pd.DataFrame(x_test_data, columns=['Sex', 'Hb'])
-predicted = logr.predict(X_test)
-print(predicted)
 
 age = st.text_input("Sex","0") 
 age = st.text_input("Hb","0")
